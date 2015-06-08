a1df0a5a243776cabc77571b7eafff25f5d37dac
Nianpeng Li <linianpeng@gmail.com>
2015-06-07 21:35:37 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  // Project configuration.
+  grunt.initConfig({
+
+    browserify: {
+      build: {
+        src: [
+          "app/app.js"
+        ],
+        dest: "public/scripts/app.js",
+        options: {
+          transform: ['reactify']
+        }
+      }
+    },
+
+    concurrent: {
+      target: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: 'app/app.js',
+        tasks: ['build'],
+        options: {
+          debounceDelay: 250,
+        },
+      },
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('build', ['browserify:build']);
+  grunt.registerTask('default', ['build', 'concurrent']);
+
+};
